4fa205be67b89313931e858abe62fc01d21a6446
Zach Wells <zach.13.wells@gmail.com>
2022-02-23 08:38:24 -0600

    Added code to scraping.py to get hemisphere image links and titles

--- scraping.py
+++ scraping.py
@@ -26,7 +26,7 @@ def scrape_all():
             'featured_image': featured_image(browser),
             'facts': mars_facts(),
             'last_modified': dt.datetime.now(),
-            'hemispheres': mars_hemispheres()
+            'hemispheres': mars_hemispheres(browser)
             }
 
     # Stop webdriver and return data
@@ -95,7 +95,31 @@ def mars_facts():
 
     return df.to_html()
 
-def mars_hemispheres():
+def mars_hemispheres(browser):
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    home_soup = soup(browser.html, 'html.parser')
+    links = home_soup.find_all('img', 'thumb')
+    links = [f"{url}{x.parent['href']}" for x in links]
+
+    for link in links:
+        browser.visit(link)
+        title = browser.find_by_css('h2.title').text
+        img_url = browser.find_by_tag('li').first.find_by_tag('a')['href']
+        
+        hemisphere_image_urls.append({'img_url': img_url, 'title': title})
+
+    browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == '__main__':
     # If running as script, print scraped data
